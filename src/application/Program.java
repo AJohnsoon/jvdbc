0b744b4b375594da028ba645659a0fe2540014d6
ajohnsoon <allanjohnson.leal@gmail.com>
2022-09-17 00:20:09 -0300

    feat: add database config

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package src.application;
+
+import src.application.db.DB;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Program {
+    public static void main(String[] args) throws SQLException {
+            Connection conn = DB.getConnection();
+            DB.closeConnection();
+    }
+}
